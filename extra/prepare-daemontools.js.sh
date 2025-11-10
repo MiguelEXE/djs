24d247d9ccc1bcb28e97a8953ed18d2790ba47fe
MiguelEXE <miguelex3@protonmail.com>
2025-11-10 22:51:56 +0000

    Fix prepare-daemontools.js.sh npm commands. Add option to patch the common.ts file from daemontools.js

--- extra/prepare-daemontools.js.sh
+++ extra/prepare-daemontools.js.sh
@@ -3,7 +3,11 @@ set -euo pipefail
 
 mkdir /etc/djs/daemontools.js
 cd /etc/djs/daemontools.js-repo
-npm run:build
+npm install
+cd src
+sed -i 's/^export const DEFAULT_SERVICE_PATH.*$/export const DEFAULT_SERVICE_PATH = "\/etc\/djs\/services"/' common.ts
+cd ..
+npm run build
 
 mv dist/* ../daemontools.js/
 cd ..
